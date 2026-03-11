40573e1692239b8154174e59759a7b374423dae9
Kohana Tripurneni <206493895+kohanatripurneni@users.noreply.github.com>
2026-03-11 12:22:53 +0530

    Add files via upload

---
+++ Training_1.py
@@ -0,0 +1,43 @@
+# math = 40
+# pi = 3.14
+# name = 'Kohana Tripurneni'
+# print(type(math))
+# print(type(pi))
+# print(type(name))
+# print(complex(3))
+# print(complex(12.5))
+# print(complex(True))
+#print(complex(False))
+# print(complex("5"))
+# print(complex("5.6"))
+# print(complex(5,-3))
+# print(complex(True,False))
+# print(bool(0))
+# print(bool(15))
+# print(bool(3.14))
+# print(bool(1+2j))
+# principal = 100000
+# roi = 10
+# time = 1
+# si = principal*roi*time/100
+# print("Simple Interest=", si)
+# val1 = 2
+# val2 = 4
+# temp = val1
+# val1 = val2
+# val2 = temp
+# print(val1)
+# print(val2)
+# h =1000
+# inch = h*12
+# cm = inch*2.54
+# print(inch)
+# print(cm)
+num = 1234567
+print(num)
+a = num % 10 #3
+num = num // 10 # num = 12
+b = num % 10 
+c = num // 10
+rev = a*100 + b*10 +c*1 
+print(rev)
